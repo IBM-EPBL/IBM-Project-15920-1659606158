c332952b5d004949ee8a26a2c3016aa8dc543bff
AbiramiArumugam15 <114007486+AbiramiArumugam15@users.noreply.github.com>
2022-09-23 03:43:10 -0700

    Add files via upload

---
+++ ASSIGNMENT/Team member 2/Assignment 1/IBM coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[23,2,3,12,29,42,17]
+print(list)
+
+#insertion
+list.insert(5,145)
+print(list)
+
+#deletion of first occurence of integer
+del list[5]
+print(list)
+
+#insertion at end of list
+list.append(27)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Happy ")
+str2=("Morning")
+print(str1+str2)
+
+#reverse of String
+str3="reverse"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,5,2)])
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ASSIGNMENT/Team member 2/Assignment 1/IBM coding output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/lenovo/Desktop/ASSIGNMENT/Team member 2/Assignment 1/IBM coding 1.py
+[23, 2, 3, 12, 29, 42, 17]
+[23, 2, 3, 12, 29, 145, 42, 17]
+[23, 2, 3, 12, 29, 42, 17]
+[23, 2, 3, 12, 29, 42, 17, 27]
+[2, 3, 12, 17, 23, 27, 29, 42]
+[2, 3, 12, 17, 23, 27, 29]
+[29, 27, 23, 17, 12, 3, 2]
+Happy Morning
+esrever
+ee
