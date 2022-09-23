5c3c4faa73bb06c1c8df56dac7b8310b59b6bffe
KowsiKaran <114061230+KowsiKaran@users.noreply.github.com>
2022-09-23 03:24:19 -0700

    Add files via upload

---
+++ ASSIGNMENT/Team member 3/Assignment 1/IBM coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[16,23,7,32,27,47,15]
+print(list)
+
+#insertion
+list.insert(5,17)
+print(list)
+
+#deletion of first occurence of integer
+del list[2]
+print(list)
+
+#insertion at end of list
+list.append(27)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Hello ")
+str2=("Everyone")
+print(str1+str2)
+
+#reverse of String
+str3="international"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,5,2)])
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ASSIGNMENT/Team member 3/Assignment 1/IBM coding output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\lenovo\Desktop\ASSIGNMENT\Team member 3\Assignment 1\IBM coding 1.py
+[16, 23, 7, 32, 27, 47, 15]
+[16, 23, 7, 32, 27, 17, 47, 15]
+[16, 23, 32, 27, 17, 47, 15]
+[16, 23, 32, 27, 17, 47, 15, 27]
+[15, 16, 17, 23, 27, 27, 32, 47]
+[15, 16, 17, 23, 27, 27, 32]
+[32, 27, 27, 23, 17, 16, 15]
+Hello Everyone
+lanoitanretni
+ne
